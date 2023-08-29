ed93a5846f38126254ab2abaf46e1555fef59b37
Mustafa Çor <corxjs@gmail.com>
2023-08-29 15:51:34 +0300

    frontend

--- backend/auth.middleware.js
+++ backend/auth.middleware.js
@@ -2,6 +2,9 @@ const jwt = require("jsonwebtoken");
 
 
 const auth = async (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(401).json({ error: "Authorization header missing" });
+    }
     try {
         const token = req.headers.authorization.split(" ")[1];
         let decodedData;
